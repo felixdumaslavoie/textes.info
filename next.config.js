0cd75c2d4a2ffc54e398e71a2e20dfb73474f44f
felixdumaslavoie <felixdumaslavoie@gmail.com>
2022-04-30 15:32:16 -0400

    Ajout de Tailwind

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
